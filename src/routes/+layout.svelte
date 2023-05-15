b90164cf7b8967fb8fef7223e35b3cb4c8df94a4
Jaydeep <sjaydeep189@gmail.com>
2023-05-15 22:18:54 +0530

    SEO added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,16 @@
 	import 'lazysizes';
 </script>
 
+<svelte:head>
+	<title>Jaydeep Sharma</title>
+	<meta
+		name="description"
+		content="Discover my web portfolio as a full stack developer. I create software for fun, work, and
+					community. Explore my diverse projects and witness my passion for crafting innovative
+					solutions that make a difference"
+	/>
+</svelte:head>
+
 <main class="main scroll-smooth bg-1 selection:bg-purple-800 selection:text-white">
 	<Navbar />
 	<slot />
