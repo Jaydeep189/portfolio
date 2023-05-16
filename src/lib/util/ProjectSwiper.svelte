e6031b19b638e92d2bd1232ab202433c43e0a7ba
Jaydeep <sjaydeep189@gmail.com>
2023-05-16 17:55:03 +0530

    final commit

--- src/lib/util/ProjectSwiper.svelte
+++ src/lib/util/ProjectSwiper.svelte
@@ -1,10 +1,26 @@
 <swiper-container
-	class=""
+	class="md:block hidden"
+	effect="cards"
+	grab-cursor="true"
+	centered-slides="true"
+	autoplay="true"
+	loop="true"
+	initial-slide="1"
+	coverflow-effect-slide-shadows="true"
+>
+	<slot />
+</swiper-container>
+
+<swiper-container
+	class="block md:hidden"
 	effect="coverflow"
 	grab-cursor="true"
 	centered-slides="true"
 	autoplay="true"
+	no-swiping="true"
+	allow-touch-move="false"
 	slides-per-view="auto"
+	loop="true"
 	coverflow-effect-rotate="50"
 	coverflow-effect-stretch="0"
 	coverflow-effect-depth="100"
